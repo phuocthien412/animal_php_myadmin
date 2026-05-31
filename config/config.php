88e276b46fdd141894a6614476135f1c8cb210c3
phuocthien412 <minhngothien2@gmail.com>
2026-05-31 16:08:24 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 class Database {
     private $host = "localhost";
-    private $db_name = "doanltw";  // Thay bằng tên database của bạn
+    private $db_name = "animal_db";  // Thay bằng tên database của bạn
     private $username = "root";      // Thay bằng username database của bạn
     private $password = "";          // Nếu có mật khẩu, hãy điền vào
     public $conn;
